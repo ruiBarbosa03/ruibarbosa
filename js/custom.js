497d72a346146fc39a5d885716919e67e4630ab5
ruiBarbosa03 <rui_barbosa03@hotmail.com>
2018-03-06 17:58:38 +0000

    Add files via upload
    First commit.
    Work in progress.

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+document.getElementById("div1").onmouseover = function() {        
+            document.getElementById("div1").style.fontSize  = "125%"
+                        };
+
+ document.getElementById("div1").onmouseout = function() {        
+            document.getElementById("div1").style.fontSize  = "100%";
+                        };
